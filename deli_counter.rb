0072e1e75fb7a0eb6e971f837adeb415c67f91cc
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-18 14:18:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     puts "The line is currently empty."
   elsif deli.length >= 1
     numbered_line = []
-    deli.each_with_index { |index, name| numbered_line << "#{index + 1}. #{name}" }
-    puts "The line is currently:" numbered_line
+    deli.each_with_index { |name, index| numbered_line << "#{index + 1}. #{name}" }
+    puts "The line is currently: #{numbered_line}"
   end
 end
