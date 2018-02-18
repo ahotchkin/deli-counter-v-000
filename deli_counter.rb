c90b94981d313accaecaad2fcbe2dc13a340664b
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-18 14:22:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 # Write your code here.
-def line(deli)
-  if deli.length == 0
+def line(deli_line)
+  if deli_line.length == 0
     puts "The line is currently empty."
-  elsif deli.length >= 1
+  elsif deli_line.length >= 1
     numbered_line = []
-    deli.each_with_index { |name, index| numbered_line << "#{index + 1}. #{name}" }
+    deli_line.each_with_index { |name, index| numbered_line << "#{index + 1}. #{name}" }
     puts "The line is currently: #{numbered_line.join(" ")}"
   end
 end
+
+def take_a_number(deli, name)
