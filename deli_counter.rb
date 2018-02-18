6c7129d6ca27760869bb9076f6cb1d62df13c263
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-18 13:46:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
-# def line(deli)
-#   if deli.length == 0
-#     puts "The line is currently empty."
-#   elsif deli.length >= 1
-#     puts
-#
-#     "The line is currently: 1. Logan 2. Avi 3. Spencer"
-#   end
-# end
+def line(deli)
+  if deli.length == 0
+    puts "The line is currently empty."
+  # elsif deli.length >= 1
+  #   puts
+  #
+  #   "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  end
+end
